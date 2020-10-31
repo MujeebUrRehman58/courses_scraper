841992a9e54eb699b8db56ca7c2e5da6fb2bc8b4
mujeeb <mrehman.bscs15seecs@seecs.edu.pk>
2020-10-31 14:55:14 +0500

    quits uploading in no data file path arg

--- courses/upload_data.py
+++ courses/upload_data.py
@@ -63,25 +63,26 @@ if __name__ == '__main__':
         data_file_path = sys.argv[1]
     except Exception as ex:
         print('Please provide json data file path as command line arg')
-    token = login()
-    if token:
-        courses = json.load(open(data_file_path))
-        for c in courses:
-            img_count = 0
-            for img in c['images']:
-                try:
-                    with open(f"{pathlib.Path(__file__).parent.absolute()}"
-                              f"/courses/spiders/{img['path']}", 'rb') as img_file:
-                        img_name = img['path'].split('full/')[1]
-                        img_string = base64.b64encode(img_file.read()).decode('utf-8')
-                        fid = upload_image(token, img_string, img_name)
-                        if fid:
-                            c['images_fids'].append(fid)
-                except Exception as ex:
-                    pass
-                img_count += 1
-                if img_count >= 5:
-                    break
-            create_course(token, c)
+    if data_file_path:
+        token = login()
+        if token:
+            courses = json.load(open(data_file_path))
+            for c in courses:
+                img_count = 0
+                for img in c['images']:
+                    try:
+                        with open(f"{pathlib.Path(__file__).parent.absolute()}"
+                                  f"/courses/spiders/{img['path']}", 'rb') as img_file:
+                            img_name = img['path'].split('full/')[1]
+                            img_string = base64.b64encode(img_file.read()).decode('utf-8')
+                            fid = upload_image(token, img_string, img_name)
+                            if fid:
+                                c['images_fids'].append(fid)
+                    except Exception as ex:
+                        pass
+                    img_count += 1
+                    if img_count >= 5:
+                        break
+                create_course(token, c)
 
 
